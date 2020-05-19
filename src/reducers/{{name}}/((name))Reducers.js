90cff0afe40b2a9a8194a30f55de9906148aadfb
Vlad <excelent91@mail.ru>
2020-05-19 18:37:34 +0300

    deleted useless code

--- src/reducers/{{name}}/((name))Reducers.js
+++
@@ -1,8 +0,0 @@
-import list from 'reducers/((name))/((name))ListReducers';
-import form from 'reducers/((name))/((name))FormReducers';
-import { combineReducers } from 'redux';
-
-export default combineReducers({
-  list,
-  form,
-});
